7431061a96e0cb81a9a0978c126d1e84265d6f0b
Lawrence <Caulker>
2020-05-24 10:59:09 -0400

    added job projects section

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,42 @@ import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 
-export default () => {
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
+
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      <Projects projects={projects} title="Featured Projects" showLink />
     </Layout>
   )
 }
-// ...GatsbyImageSharpFluid
